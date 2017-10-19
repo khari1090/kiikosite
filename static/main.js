10518ab256f22b43fbcf9f5c785b831167b91047
Khari <khari1090@gmail.com>
2017-10-19 21:28:53 +0200

    redesign

--- static/main.js
+++ static/main.js
@@ -5,4 +5,19 @@ function myFunction(){
   } else {
     x.className = "topnav";
   }
+
+  var myNav = document.getElementById('myTopnav');
+  window.onscroll = function() {
+      "use strict";
+      if (document.body.scrollTop >= 200 ) {
+          myNav.classList.add(".nav-colored");
+          myNav.classList.remove(".nav-transparent");
+          console.log('white');
+      }
+      else {
+          myNav.classList.add(".nav-transparent");
+          myNav.classList.remove(".nav-colored");
+          console.log('trans');
+      }
+  };
 }
