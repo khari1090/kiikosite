64456f0a40961aecfa5179daa4e08778ef6f12ce
Khari <khari1090@gmail.com>
2017-10-20 09:37:22 +0200

    minus js

--- static/main.js
+++ static/main.js
@@ -5,16 +5,3 @@ function myFunction(){
   } else {
     x.className = "topnav";
   }
-
-  window.onscroll = function scroll(e) {
-    var vertical_position = 0;
-    if (pageYOffset)//usual
-      vertical_position = pageYOffset;
-    else if (document.documentElement.clientHeight)//ie
-      vertical_position = document.documentElement.scrollTop;
-    else if (document.body)//ie quirks
-      vertical_position = document.body.scrollTop;
-  console.log( vertical_position);
-    var your_div = document.getElementById('img-hero');
-    your_div.style.top = (vertical_position + 20) + 'px';//200 is arbitrary.. just to show you could now position it how you want
-  }
